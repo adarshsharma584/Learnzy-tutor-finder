7023951a5375da838ace4f56b0503b67d6c184a0
adarshsharma584 <adarshsharma0862@gmail.com>
2025-10-31 22:59:45 +0530

    add security middlewares

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,10 @@ import { ConnectDb } from "./src/utils/dbConnection.js";
 import CookieParser from "cookie-parser"
 import config from "./src/config/index.js";
 import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
-
+import helmet from "helmet";
+import cors from "cors";    
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 
 const app = express ();
 const PORT = config.PORT;
@@ -11,6 +14,16 @@ const PORT = config.PORT;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(CookieParser());
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(
+    rateLimit({
+        windowMs: 10 * 60 * 1000, // 10 minutes
+        max: 100,
+    })
+);
+
 
 ConnectDb()
 .then(() => {
